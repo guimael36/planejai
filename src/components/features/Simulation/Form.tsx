ee73455ad87319ceb110da6f5d4bc61fc7495fd2
Guilherme Amaral <guilherme.amaral9@hotmail.com>
2026-07-14 17:27:44 -0300

    feat: cria o card de pergunta e o input do formulario

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,17 @@
-import { StepProgress } from "./Progress"
+import { StepProgress } from './Progress'
+import { FormStep } from './FormStep'
+import { PiggyBank } from 'lucide-react'
 
 export const SimulationForm = () => {
   return (
     <>
       <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?"
+        inputProps={{type: 'text', placeholder: 'ex: 5.000,00', prefix: 'R$'}}
+      />
     </>
   )
 }
