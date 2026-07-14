de62f5f90ce61d5b5b67abb10c757e5dfcb21018
Guilherme Amaral <guilherme.amaral9@hotmail.com>
2026-07-14 15:25:13 -0300

    feat: adiciona contexto de tema, salvo no localStorage, com botao de troca no Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { ThemeProvider } from './context/theme/ThemeProvider'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
