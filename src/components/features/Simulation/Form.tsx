cbab13ffbd5db88176263fbed7257e3a94fb04e6
Guilherme Amaral <guilherme.amaral9@hotmail.com>
2026-07-14 17:47:18 -0300

    feat: adiciona a interface basica de passos do formulario

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,17 +1,14 @@
 import { StepProgress } from './Progress'
 import { FormStep } from './FormStep'
-import { PiggyBank } from 'lucide-react'
+import { simulationFormSteps } from '@/data/simulation'
 
 export const SimulationForm = () => {
+  const currentStep = simulationFormSteps[0]
+
   return (
     <>
       <StepProgress currentStep={1} totalSteps={6} />
-      <FormStep
-        icon={PiggyBank}
-        title="Renda mensal bruta"
-        question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?"
-        inputProps={{type: 'text', placeholder: 'ex: 5.000,00', prefix: 'R$'}}
-      />
+      <FormStep key={currentStep.id} {...currentStep}/>
     </>
   )
 }
